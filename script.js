21db5db4c53070ceb462ea48a910a44838ff3c2a
Tanyabatra541 <73140301+Tanyabatra541@users.noreply.github.com>
2022-01-16 22:29:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeColorOfHeart(){
+    document.getElementById("heart").style.color="red";
+}
+
+function play() {
+    var music= document.getElementById("music");
+    music.play();
+}
+
+function pause() {
+    var music= document.getElementById("music");
+    music.pause();
+}
+
+
